bd75edc232fb2d1892e9e1a69a1d1e1d700ac763
HenriqueRosa <henriquerodrigues89@hotmail.com>
2022-03-23 19:37:35 -0300

    .

---
+++ CursoJs/Aula11_Operacoes/11_decimaprimeira.js
@@ -0,0 +1,12 @@
+var n1 = 10;
+var n2 = 20;
+var soma = n1 + n2;
+
+console.log(soma);
+
+function somar(a, b) {
+    return a + b;
+
+}
+
+console.log(somar(2, 5));
---
+++ CursoJs/Aula12/12_decimasegundaaula.js
@@ -0,0 +1,11 @@
+const a = 10;
+const b = 33;
+
+function somar(a, b) {
+    return a + b;
+}
+
+document.querySelector("#calcular").addEventListener("click", function () {
+
+    alert("Timão Campeão");
+});
